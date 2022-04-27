ceca4b3a6a25a0a02e096b67084b9c78bcfd338e
Barry <barry@speedchess.club>
2022-04-27 12:03:25 -0500

    main

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
     title: `The SpeedChess.Club`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://speedchessclub.flywheelsites.com`
   },
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
       "url": "https://speedchessclub.flywheelsites.com/graphql"
-    }
+    },
+    resolve: 'gatsby-plugin-netlify',
   }]
 };
